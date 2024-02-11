333c15e690ba12e4e8c34b85d0abd429f241b95d
nantawoot101 <mosso45180@gmail.com>
2024-02-11 13:39:33 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+    <h1 className="text-lg text-pink-600 text-center border bg-lime-500 py-5 font-Mitr">
+      Hello world!
+    </h1>
+    <div data-theme="dark">
+    <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Mitr', 'sans-serif']
+
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+}
